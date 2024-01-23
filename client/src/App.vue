9efbe32b16c9ab242406a46219c52867bd9a8bb3
LA <no>
2024-01-23 22:06:04 +0500

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+let data = {
+  message: "Demo"
+}
+
+</script>
+
+<template>
+  <div class="text-5xl text-center mt-10">
+  {{ data.message }}
+  </div>
+  <RouterView />
+</template>
+
